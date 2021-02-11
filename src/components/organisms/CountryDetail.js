d39df8b0de08740a09f25ac59df9160b122250cf
Celiamf <celiamartinfern@gmail.com>
2021-02-11 21:48:04 +0100

    [Countrydetails] Format country & embassy containers

--- src/components/organisms/CountryDetail.js
+++ src/components/organisms/CountryDetail.js
@@ -7,30 +7,39 @@ const CountryDetail = () => {
   return (
     <>
       {/* <BackToListLink /> */}
-      <article className="detail">
-        <div>
-          <h2>Ghana</h2>
-          <img src={GhanaFlag} alt="Ghana" className="detail__img" />
+      <article className="detailCardContainer">
+        <div className="countryContainer">
+          <img src={GhanaFlag} alt="Ghana" className="countryFlag" />
+          <h2 className="countryTitle">Ghana</h2>
         </div>
-        <div>
-          <h2>Embajada en Madrid</h2>
-          <ul className="detail__EmbassyInfo">
-            <li></li>
+        <div className="embassyContainer">
+          <h2>Embajada</h2>
+          <ul>
             <li className="detail__EmbassyInfo detail__EmbassyInfo--address">
               <a
                 href="https://goo.gl/maps/FFXMU9uj7zr8UVpPA"
                 target="_blank"
                 title="See Embassy's address on Google"
-              ></a>
+              >
+                Dirección
+              </a>
             </li>
             <li className="detail__EmbassyInfo detail__EmbassyInfo--phone">
               915 670 440
             </li>
             <li className="detail__EmbassyInfo detail__EmbassyInfo--email">
-              consular@ghanaembassy.es
+              <a href="mailto:consular@ghanaembassy.es">
+                consular@ghanaembassy.es
+              </a>
             </li>
             <li className="detail__EmbassyInfo detail__EmbassyInfo--web">
-              https://ghanaembassy.es/passport-application/
+              <a
+                href="https://ghanaembassy.es/passport-application/"
+                target="_blank"
+                title="Go to Embassy's website"
+              >
+                Web
+              </a>
             </li>
           </ul>
         </div>
